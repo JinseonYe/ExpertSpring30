c29a5edd2bed3bc253c247127f3afb2b9ff1ebaa
JinseonYe <aatwe24@gmail.com>
2023-07-28 02:48:58 +0900

    [Feat] 생성자를 통한 의존성 주입

---
+++ src/main/java/com/example/expertspring30/expert001_02/Driver.java
@@ -0,0 +1,11 @@
+package com.example.expertspring30.expert001_02;
+
+public class Driver {
+	public static void main(String[] args) {
+		Tire tire = new KoreaTire();
+		// Tire tire = new AmericaTire();
+		Car car = new Car(tire);
+
+		System.out.println(car.getTireBrand());
+	}
+}
